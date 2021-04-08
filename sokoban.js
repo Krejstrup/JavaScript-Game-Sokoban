4cf15cf8640d950280a4e367319d074eeee3f471
Richard Krejstrup <krejstrup@hotmail.com>
2021-04-08 14:56:13 +0200

    Added som functionality to the map selection

--- sokoban.js
+++ sokoban.js
@@ -5,55 +5,93 @@ let gameDone = false;
 // Create an event-listener for keys, and call keyPressed()
 document.addEventListener("keydown", keyPressed);
 
-// this is a startup function that initiates from the body - don't change!!
+// This is a startup function that initiates from the body - don't change!!
 function startUp(){
-    // predefined: gameField = tileMap01 - In later game patch: get the game field from a player select
-    const playgroundElement = document.getElementById("playGround");
-    let marker = " ";
-    let floorMarker = "N";
-    let idTag = 0; // Use CSS id to be able to find an game element faster.
+    // HOLD YER HORSES! Heres the patch 26 with selection of more games!
+    let gameSelector = document.getElementById("selGame")
+    if (gameSelector.length === 0){
+        for (let index = 0; index < allGames.length; index++) {
+            let theOption = document.createElement("option");
+            theOption.text = allGames[index].name;
+            gameSelector.add(theOption);
+            //console.log("The gameselector Index: " + index);
+        }
+    }
+    // Selector is filled. Read the one to play
+    //gameField is used as a global member - do not remove,
+    gameField = allGames[gameSelector.selectedIndex];
+    //gameField = gameSelector.value; // Got the string in the selector
+    console.log("från selectorn: " +gameSelector.selectedIndex + " med typen: " + typeof gameSelector.selectedIndex);
+    console.log("and then the selected gameField: " + gameField.name);
 
-    for (let i=0; i<gameField.height; i++){
-        for (let j=0; j<gameField.width; j++){
+    const playgroundElement = document.getElementById("playGround");
+    
+    ///====== Now, because this is function become the standard game map loader:
+    ///====== the playfield has to be erased if we are going to change the map.
+    let noPlayFieldElements = playgroundElement.childElementCount;
+    //console.log("No of <divs>: " + noPlayFieldElements);
+    
+    if (noPlayFieldElements > 0){   // tests if there is a playfield allready: erase it.
+        for (let index = 0; index < noPlayFieldElements; index++) {
+            console.log("Removing child: "+index);
+            document.getElementById(String(index)).remove();
+        }
+        //console.log("Erased, now: "+ playgroundElement.childElementCount);
+    }
+    //========= Now we are ready to (re-)draw the map
+    drawMap(playgroundElement, gameField);
+    
+    // Prepping that the game is not finished/won yet
+    gameDone = false;
+    // Resetting the number of game movements (the variable is named wrong!!)
+    keyInputs = 0;
+}
+
+
+function drawMap(gameMapParent, mapData)  // to use in later update of the game - select between different game tables.
+{
+    let marker = " ";
+    let floorMarker = "N";  // This is used to minimize the repeating: marker = "N";
+    let idTag = 0;          // Use CSS id to be able to find an game element faster.
+    console.log("Draws the map");
+
+    for (let i=0; i<mapData.height; i++){
+        for (let j=0; j<mapData.width; j++){
             
-            marker = gameField.mapGrid[i][j]; // Fetch gamedata
+            marker = mapData.mapGrid[i][j]; // Fetch gamedata - zero because we only use one string
             let newDivBox = document.createElement("div");
             newDivBox.classList.add("box");   // All are boxes
-
+    
             if (marker == " "){
                 newDivBox.classList.add(floorMarker); // this is a floor tile
-
+    
             } else if (marker == "B") {
                 newDivBox.classList.add(floorMarker); // this is a moving element (B) over floor
-                marker = "B";
-                newDivBox.classList.add(marker);
-
-            } else if (marker == "GB"){
+                newDivBox.classList.add(marker);    //marker is "B"
+    
+            } else if (marker == "GB")  // GB is special: if a box is placed over a Goal marker
+            {
                 marker = "G";
                 newDivBox.classList.add(marker);
                 marker = "B";
                 newDivBox.classList.add(marker);
-
-            } else if (marker == "P") {
+    
+            } else if (marker == "P") 
+            {
                 newDivBox.classList.add(floorMarker);
-                marker = "P";   // this is a moving player (P) element over floor
-                newDivBox.classList.add(marker);
+                newDivBox.classList.add(marker);    //marker is "P"
                 playerPos = idTag;  // idTag for this gamer element will be saved here
-
+    
             } else{
-                // This is the other tiles
+                // This is the other tiles, that are not "special", just copy.
                 newDivBox.classList.add(marker);
             }
             newDivBox.setAttribute("id", idTag++);
-            playgroundElement.appendChild(newDivBox);
+            gameMapParent.appendChild(newDivBox);
         } 
     }
-    gameDone = false;
-    keyInputs = 0;
 }
-function SelGame(game){
-    console.log("This is game. Selected: " +game);
-}
+
 function movePlayer(dudePos,nextPos)    // tex Player start pos and up: (220,-19)
 {
     let playgroundElement = document.getElementById(dudePos+nextPos); // Get one element ahead
